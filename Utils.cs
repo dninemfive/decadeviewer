6aa771ffd17bf6bace459eea6ba04a1d6ab7fe48
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-04-25 16:57:01 -0700

    code documentation and cleanup, part 3

--- Utils.cs
+++ Utils.cs
@@ -9,6 +9,12 @@ namespace DecadeViewer
 {
     public static class Utils
     {
+        /// <summary>
+        /// Finds all the files in a given folder, if they are accessible. Used instead of <see cref="Directory.EnumerateFiles"/>
+        /// directly because the latter errors when it encounters Windows library folders. 
+        /// </summary>
+        /// <param name="path">The base path of the folder whose files will be read.</param>
+        /// <returns>Every accessible absolute path corresponding to a file in this folder or a subfolder at any depth.</returns>
         // wish i didn't have to copy and paste this code everywhere lmao
         public static IEnumerable<string> AllFilesRecursive(this string path)
         {
@@ -32,6 +38,9 @@ namespace DecadeViewer
             }
             foreach (string file in Directory.EnumerateFiles(path)) yield return file;
         }
+        /// <param name="file">The file whose decade will be found.</param>
+        /// <returns>The plaintext name of the year in which the song corresponding to <c>file</c> was performed, or "uncategorized"
+        ///  if that value is unset (0).</returns>
         public static string Decade(this TagLib.File file)
         {
             uint year = file.Tag.Year;
--- WeightMethod.cs
+++ WeightMethod.cs
@@ -7,15 +7,39 @@ using TagLib;
 
 namespace DecadeViewer
 {
+    /// <summary>
+    /// Base class for <see cref="WeightMethod_Song"/>, <see cref="WeightMethod_Album"/>, <see cref="WeightMethod_Duration"/>, &c.
+    /// Holds the behavior of each aspect of song weighting and makes it available to render in the UI.
+    /// </summary>
     public abstract class WeightMethod
     {
-        public static readonly WeightMethod Song = new WeightMethod_Song(), 
-                                            Album = new WeightMethod_Album(),
-                                            Duration = new WeightMethod_Duration();
+        /// <summary>
+        /// The canonical instance of <see cref="WeightMethod_Song"/>.
+        /// </summary>
+        public static readonly WeightMethod_Song Song = new();
+        /// <summary>
+        /// The canonical instance of <see cref="WeightMethod_Album"/>.
+        /// </summary>
+        public static readonly WeightMethod_Album Album = new();
+        /// <summary>
+        /// The canonical instance of <see cref="WeightMethod_Duration"/>.
+        /// </summary>
+        public static readonly WeightMethod_Duration Duration = new();
+        /// <summary>
+        /// Contains one instance of each working subclass of <c>WeightMethod</c>.
+        /// </summary>
         public static readonly List<WeightMethod> AllMethods = new() { Song, Album, Duration };
+        /// <summary>
+        /// The name of the weight method, used to show it in the selection dropdown.
+        /// </summary>
         public virtual string Name { get; }
+        /// <returns><see cref="Name"/></returns>
         public override string ToString() => Name;
+        /// <param name="file">The <see cref="TagLib.File"/> whose weight to calculate.</param>
+        /// <returns>The proportion of its parent progress bar that this <c>file</c> will fill up.</returns>
         public abstract double Weight(TagLib.File file);
-        public abstract string Format(DecadeEntry de);
+        /// <param name="decadeEntry">The <see cref="DecadeEntry"/> whose summary to produce.</param>
+        /// <returns>A human-readable summary of the contents of the <c>decadeEntry</c>.</returns>
+        public abstract string Format(DecadeEntry decadeEntry);
     }    
 }
